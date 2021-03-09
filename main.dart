f2020747bbe0ce8c64ab3c1e9481e09c169f9cd2
Ashraf Otagun <Otagunolaniyi7@gmail.com>
2021-03-09 15:42:43 -0800

    Added something to approx the final amount down, just to be more realistic

--- main.dart
+++ main.dart
@@ -24,6 +24,7 @@ void main() {
     double percent = 15 * initialAmount / 100; // a 15% interest rate per month
     initialAmount += percent;
     finalAmount = initialAmount;
+    finalAmount = finalAmount.floorToDouble();
   });
   print(
       'Guy, when you invest 4000 naira for 12 months at a 15% interest rate per month, you make: $finalAmount naira');
