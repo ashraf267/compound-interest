4e40812a915ed2685cb2d121e69d616c0edd20ce
Ashraf Otagun <Otagunolaniyi7@gmail.com>
2021-03-09 15:19:18 -0800

    Added a 6 months investment duration

--- main.dart
+++ main.dart
@@ -1,5 +1,7 @@
 void main() {
   // a simple compound interest demo program
   // assume a dollar has a 400 naira equivalent
+  // assume the investment last for 6 months
   double actualAmount = 4000; // a dollar
+  List<String> months = ['Jan', 'Feb', 'March', 'April', 'May', 'June'];
 }
